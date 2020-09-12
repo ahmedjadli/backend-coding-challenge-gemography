d752193f49f6e120c3e25c47ba6e610a3bdc5be9
ahmedjadli <ahmedjadli25@gmail.com>
2020-09-12 18:23:21 +0100

    v1 algorithm of recalculation by language

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,11 +1,43 @@
 const axios = require("axios");
-const { all } = require("../app");
 const baseURL =
   "https://api.github.com/search/repositories?q=created:>{date}&sort=stars&order=desc";
+
+class Language {
+  constructor(name, repos, count) {
+    this.name = name;
+    this.repos = repos;
+    this.count = count;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getRepos() {
+    return this.repos;
+  }
+
+  getCount() {
+    return this.count;
+  }
+
+  setName(name) {
+    this.name = name;
+  }
+
+  setRepos(repos) {
+    this.repos = repos;
+  }
+
+  setCount(count) {
+    this.count = count;
+  }
+}
 
 const fetchGithub = async () => {
   let allRepos = [];
 
+  // fetch all repos
   allRepos = await axios
     .get(
       "https://api.github.com/search/repositories?q=created:>2020-07-11&sort=stars&order=desc&per_page=100"
@@ -14,13 +46,37 @@ const fetchGithub = async () => {
       return res.data.items;
     });
 
+  // extract distinct languages name from repos array
   let Languages = [
-    ...new Set(allRepos.map((repo) => repo.language != null && repo.language)),
+    ...new Set(
+      allRepos.map((repo) => repo.language).filter((repo) => repo !== null)
+    ),
   ];
 
-  let jsRepos = [...allRepos.filter((repo) => repo.language !== "Kotlin")];
+  console.log(allRepos.length);
 
-  console.log(jsRepos.length);
+  console.log(Languages.length);
+
+  let LangArr = [
+    ...new Set(
+      Languages.map((lang) => {
+        let repos = [];
+        let count = 0;
+        allRepos.map((repo) => {
+          if (repo.language === lang) {
+            repos.push(repo);
+            count++;
+          }
+        });
+        return new Language(lang, repos, count);
+      })
+    ),
+  ];
+
+  console.log(LangArr.length);
+
+  let countt = LangArr.reduce((a, b) => a + b.count, 0);
+  console.log(countt);
 };
 
 fetchGithub();
